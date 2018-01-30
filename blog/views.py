44ffdef0ca935c10c7194e4327142f35d9f00261
Suhyun <supr2000@gmail.com>
2018-01-30 16:57:26 +0900

    My Django Girls app,

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+#from .models import post_list
 # Create your views here.
 
 def post_list(request):
-	return render(request, 'blog/post_list.html',{})
+	Post.objects.filter(published_data__lte=timezone.now()).order_by('published_data')
+	return render(request, 'blog/post_list.html',{'posts':posts})
